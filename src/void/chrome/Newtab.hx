c10a9c0d6c26c63c0de1179a0a64d1cc360ddd9b
tong <tong@disktree.net>
2016-10-21 13:50:24 +0200

    Empty chrome-newtab placeholder

---
+++ src/void/chrome/Newtab.hx
@@ -0,0 +1,16 @@
+package void.chrome;
+
+import js.Browser.document;
+import js.Browser.window;
+import chrome.Bookmarks;
+import chrome.History;
+import chrome.Runtime;
+
+/**
+*/
+class Newtab {
+
+	static function main() {
+		document.body.textContent = "DISKTREE.NET";
+	}
+}
